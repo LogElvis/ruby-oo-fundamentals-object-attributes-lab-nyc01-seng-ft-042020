8328bf633107230380fc9d8a368c26043e52f166
LogElvis <ningelvis@gmail.com>
2020-04-21 16:43:46 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+    def name #getter method
+        @name
+    end
+    
+    def name=(name) #setter method
+        @name = name
+    end
+
+    def breed
+        @breed
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+end
